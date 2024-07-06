7a6e7583d573e47ab4a9f1143cd912486ec56144
azizrahimov2509 <rahimov2509@mail.com>
2024-07-06 19:56:46 +0300

    adminpanel

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,6 +3,7 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../farebase/config";
 import { useNavigate } from "react-router-dom";
 import { Form, Input, Button, Row, Col, Typography, Alert } from "antd";
+import Img from "../../../public/Wall-Panels-9.jpg"; // Проверьте путь к изображению
 
 const { Title } = Typography;
 
@@ -47,100 +48,105 @@ const Login: React.FC = () => {
   };
 
   return (
-    <Row style={{ minHeight: "100vh", backgroundColor: "#001f3f" }}>
-      <Col
-        span={12}
-        style={{
-          backgroundColor: "#001f3f",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "48px",
-          color: "#fff",
-        }}
-      >
-        <div
+    <>
+      <Row style={{ minHeight: "100vh", backgroundColor: "#001f3f" }}>
+        <Col
+          span={12}
           style={{
-            maxWidth: "500px",
-            width: "100%",
-            padding: "24px",
-            backgroundColor: "#001529",
-            borderRadius: "8px",
-            boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
-            border: "1px solid #d9d9d9",
+            backgroundColor: "#001f3f",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "48px",
             color: "#fff",
           }}
         >
-          <Title level={2} style={{ textAlign: "center", color: "#fff" }}>
-            Admin Panel
-          </Title>
-          <Form
-            name="login"
-            initialValues={{ remember: true }}
-            onSubmitCapture={handleLogin}
+          <div
+            style={{
+              maxWidth: "500px",
+              width: "100%",
+              padding: "24px",
+              backgroundColor: "#001529",
+              borderRadius: "8px",
+              boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
+              border: "1px solid #d9d9d9",
+              color: "#fff",
+            }}
           >
-            <div className="flex items-center justify-center ">
-              <div className=" flex flex-col items-end justify-center">
-                <Form.Item
-                  label={<span style={{ color: "#fff" }}>Email</span>}
-                  name="email"
-                  rules={[
-                    { required: true, message: "Please input your email!" },
-                  ]}
-                >
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="Enter your email..."
-                    value={loginData.email}
-                    onChange={handleChange}
-                    className="w-52"
-                  />
-                </Form.Item>
+            <Title level={2} style={{ textAlign: "center", color: "#fff" }}>
+              Admin Panel
+            </Title>
+            <Form
+              name="login"
+              initialValues={{ remember: true }}
+              onSubmitCapture={handleLogin}
+            >
+              <div className="flex items-center justify-center ">
+                <div className=" flex flex-col items-end justify-center">
+                  <Form.Item
+                    label={<span style={{ color: "#fff" }}>Email</span>}
+                    name="email"
+                    rules={[
+                      { required: true, message: "Please input your email!" },
+                    ]}
+                  >
+                    <Input
+                      id="email"
+                      type="email"
+                      placeholder="Enter your email..."
+                      value={loginData.email}
+                      onChange={handleChange}
+                      className="w-52"
+                    />
+                  </Form.Item>
 
-                <Form.Item
-                  label={<span style={{ color: "#fff" }}>Password</span>}
-                  name="password"
-                  rules={[
-                    { required: true, message: "Please input your password!" },
-                  ]}
-                >
-                  <Input.Password
-                    id="password"
-                    placeholder="Enter your password..."
-                    value={loginData.password}
-                    onChange={handleChange}
-                    className="w-52"
-                  />
-                </Form.Item>
+                  <Form.Item
+                    label={<span style={{ color: "#fff" }}>Password</span>}
+                    name="password"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please input your password!",
+                      },
+                    ]}
+                  >
+                    <Input.Password
+                      id="password"
+                      placeholder="Enter your password..."
+                      value={loginData.password}
+                      onChange={handleChange}
+                      className="w-52"
+                    />
+                  </Form.Item>
+                </div>
               </div>
-            </div>
 
-            {error && (
-              <Alert
-                message={error}
-                type="error"
-                showIcon
-                style={{ marginBottom: "16px" }}
-              />
-            )}
+              {error && (
+                <Alert
+                  message={error}
+                  type="error"
+                  showIcon
+                  style={{ marginBottom: "16px" }}
+                />
+              )}
 
-            <Form.Item>
-              <Button type="primary" htmlType="submit" block>
-                LOGIN
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      </Col>
-      <Col
-        span={12}
-        style={{
-          background: `url('../../public/IMG_7006.jpg') no-repeat center center`,
-          backgroundSize: "cover",
-        }}
-      />
-    </Row>
+              <Form.Item>
+                <Button type="primary" htmlType="submit" block>
+                  LOGIN
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Col>
+        <Col
+          span={12}
+          style={{
+            background: `url(${Img}) no-repeat center center`,
+            backgroundSize: "cover",
+          }}
+        />
+      </Row>
+    </>
   );
 };
 
